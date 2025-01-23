8ff859a95b250c395b270280ce190fb43bc020e0
Sowrov9 <sowrovahammed00@gmail.com>
2025-01-24 03:35:30 +0600

    top vendor section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SelectCategory from '../SelectCategory/SelectCategory'
 import Installment from '../Installment/Installment'
+import TopVendors from '../TopVendors/TopVendors'
 
 const Home = () => {
     return (
         <>
             <SelectCategory></SelectCategory>
             <Installment></Installment>
+            <TopVendors></TopVendors>
 
         </>
     )
