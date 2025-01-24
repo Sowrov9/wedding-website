a63bcee0bddd6a50ff899c8e1eb8867233e9e852
Sowrov9 <sowrovahammed00@gmail.com>
2025-01-25 01:09:20 +0600

    Commit name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,58 +3,33 @@
 const Header = () => {
   return (
     <>
-    <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <a className="btn btn-ghost text-xl"><img src="../../../public/assets/img/logo.svg" alt="" /></a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><a>Installment</a></li>
+            <li><a>Categories</a></li>
+            <li><a>Download app</a></li>
+            <li><a>FAQ</a></li>
+            <li><a>Contact Us</a></li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">
+            <svg
+              className="swap-off fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              viewBox="0 0 512 512">
+              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+            </svg>
+            <span>Menu</span>
+          </a>
+        </div>
       </div>
-      <ul
-        tabIndex={0}
-        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
     </>
   )
 }
